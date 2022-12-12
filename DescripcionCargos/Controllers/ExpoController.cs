5c554ddc563d39449bdb52b118ba381603df18e5
Carlos Ceballos <carlosc@leadboxhq.com>
2022-12-12 07:20:29 -0400

    Add expo QR page and controller

---
+++ DescripcionCargos/Controllers/ExpoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DescripcionCargos.Controllers
+{
+	public class ExpoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
